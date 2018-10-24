c4c57a404e6a807e232ae864a0e95de468179e6d
Tianlikai <1024998526@qq.com>
2018-10-24 11:22:38 +0800

    添加jquery

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,32 @@
-const webpack = require('webpack');
-const path = require('path');
-const srcDir = path.join(__dirname, './src');
-const distDir = path.join(__dirname, './dist');
+const webpack = require("webpack");
+const path = require("path");
+const srcDir = path.join(__dirname, "./src");
+const distDir = path.join(__dirname, "./dist");
 
-const env = process.env.NODE_ENV.trim()
+const env = process.env.NODE_ENV.trim();
 
 module.exports = {
-    mode: env === 'dev' ? 'development' : 'production',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(distDir),
-        filename: env === 'dev' ? '[name].js' : '[name].min.js',
-    },
-    devtool: env === 'dev' ? 'source-map' : false,
-    module: {
-        rules: [
-            { 
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    }
-}
+  mode: env === "dev" ? "development" : "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(distDir),
+    filename: env === "dev" ? "[name].js" : "[name].min.js"
+  },
+  devtool: env === "dev" ? "source-map" : false,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    })
+  ]
+};
